275ceae253dbfd90f9fcae656effdfdd211c2e31
sauravkumar <saurav.kumar@niyotail.com>
2021-09-17 03:31:11 +0530

    initilize mongodb database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ormConfig } from './database/config/ormconfig';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    MongooseModule.forRoot(process.env.MONGO_DB_CONNECTION)    
   ],
+
   controllers: [AppController],
   providers: [AppService],
 })
