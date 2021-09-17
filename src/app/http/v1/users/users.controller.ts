8ca669f8630706131ff3d3fb09b861a5934969b9
sauravkumar <saurav.kumar@niyotail.com>
2021-09-17 21:31:45 +0530

    added Transform interceptor

--- src/app/http/v1/users/users.controller.ts
+++ src/app/http/v1/users/users.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, UseInterceptors } from '@nestjs/common';
+import { TransformInterceptor } from 'src/app/interceptors/transform.interceptor';
 import { User } from 'src/app/schemas/users/user.schema';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
@@ -6,7 +7,7 @@ import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) { }
 
   @Get(':userId')
   async getUser(@Param('userId') userId: string): Promise<User> {
@@ -14,17 +15,20 @@ export class UsersController {
   }
 
   @Get()
-  async getUsers(): Promise<User[]> {
-      return this.usersService.getUsers();
+  @UseInterceptors(TransformInterceptor)
+  async getUsers(): Promise<any> {
+    const result = await this.usersService.getUsers();
+    const meta = { version: 1.1 }
+    return { message: "test", result, meta };
   }
 
   @Post()
   async createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
-      return this.usersService.createUser(createUserDto.email, createUserDto.age)
+    return this.usersService.createUser(createUserDto.email, createUserDto.age)
   }
 
   @Patch(':userId')
   async updateUser(@Param('userId') userId: string, @Body() updateUserDto: UpdateUserDto): Promise<User> {
-      return this.usersService.updateUser(userId, updateUserDto);
+    return this.usersService.updateUser(userId, updateUserDto);
   }
 }
--- src/app/http/v1/v1.module.ts
+++ src/app/http/v1/v1.module.ts
@@ -3,9 +3,6 @@ import { RouterModule, Routes } from '@nestjs/core';
 import { UsersModule } from './users/users.module';
 import { v1Routes } from './v1.route';
 
-
-
-
 @Module({
   imports: [
     RouterModule.register(v1Routes),
