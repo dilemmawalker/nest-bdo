08f9b55acc8ec21443a24e32535cdb3edb7f2b23
sauravkumar <saurav.kumar@niyotail.com>
2021-09-18 13:20:54 +0530

    updated wiston configuration file

--- src/config/wiston.config.ts
+++ src/config/wiston.config.ts
@@ -1,22 +1,54 @@
 import * as winston from "winston";
 const path = require("path");
+
+
+enum logLevel {
+    error = "error",
+    debug = "debug",
+    warn = "warn",
+    info = "info"
+}
+class WistonConfig {
+    level: logLevel;
+    filename: string;
+    appendPath: string;
+
+    constructor(level: logLevel, filename: string, appendPath = "") {
+        this.level = level;
+        this.filename = filename;
+        this.appendPath = appendPath
+    }
+}
+
+export function getMaxLogFileSize(): any {
+    const maxLogSize = process.env.MAX_LOG_FILE_SIZE || 1000000000;
+    return maxLogSize;
+}
+
 export function wistonConfig(): object {
+    const wistonMainConfig = [new WistonConfig(logLevel.error, "error.log"),
+    new WistonConfig(logLevel.debug, "debug.log"),
+    new WistonConfig(logLevel.info, "info.log")];
+    return getConfig(wistonMainConfig);
+}
+
+function getConfig(wistonConfig: WistonConfig[]): object {
+    const transports = [];
+    wistonConfig.forEach(element => {
+        transports.push(new winston.transports.File({
+            dirname: path.join(__dirname, '../storage/logs/', element.appendPath),
+            filename: element.filename,
+            level: element.level,
+            maxsize: parseInt(getMaxLogFileSize()),
+            maxFiles: 1
+        })
+        )
+    });
     return {
         format: winston.format.combine(
-          winston.format.timestamp(),
-          winston.format.json(),
+            winston.format.timestamp(),
+            winston.format.json(),
         ),
-        transports: [
-          new winston.transports.File({
-            dirname: path.join(__dirname, '../storage/logs/'), 
-            filename: 'debug.log',
-            level: 'debug',
-          }),
-          new winston.transports.File({
-            dirname: path.join(__dirname, '../storage/logs/'),
-            filename: 'error.log',
-            level: 'error',
-          }),
-        ],
-      }
+        transports: transports,
+    }
 }
