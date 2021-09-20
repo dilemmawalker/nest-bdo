d18aef60793bff15be14724dde1a9982709d6d3d
sauravkumar <saurav.kumar@niyotail.com>
2021-09-20 11:12:07 +0530

    init all stuctural changes

--- apps/agent/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { AgentModule } from './agent.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AgentModule);
-  await app.listen(3000);
-}
-bootstrap();
---
+++ apps/api/src/app/http/users/requests/update-user.request.ts
@@ -0,0 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { UserDto } from '../../../../../../../libs/core/users/src/dtos/user.dto';
+export class UpdateUserRequest {
+    @ApiProperty()
+    email: string;
+    @ApiProperty()
+    age: number;
+
+    static getUserDto(updateUserRequest: UpdateUserRequest) {
+            const userDto = new UserDto();
+            userDto.email = updateUserRequest.email;
+            userDto.age = updateUserRequest.age;
+            return userDto;
+      }
+}
