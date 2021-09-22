eb1792a82834bd9126637a5a18bf160afd7d985b
sauravkumar <saurav.kumar@niyotail.com>
2021-09-23 02:07:32 +0530

    added jwt in swagger

--- libs/shared/config/swagger.config.ts
+++ libs/shared/config/swagger.config.ts
@@ -1,7 +1,15 @@
-import { DocumentBuilder } from "@nestjs/swagger";
+import { DocumentBuilder } from '@nestjs/swagger';
 
 export const swaggerConfig = new DocumentBuilder()
-.setTitle('Onboarding Api')
-.setDescription('The api hub for onboarding applications')
-.setVersion('1.0')
-.build();
+  .setTitle('Onboarding Api')
+  .setDescription('The api hub for onboarding applications')
+  .setVersion('1.0')
+  .addBearerAuth({
+    type: 'http',
+    scheme: 'bearer',
+    bearerFormat: 'JWT',
+    name: 'JWT',
+    description: 'Enter JWT token',
+    in: 'header',
+  })
+  .build();
