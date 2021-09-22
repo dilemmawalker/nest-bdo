6afe3b3da58a6c7d70866aaa875a39299b2838d9
sauravkumar <saurav.kumar@niyotail.com>
2021-09-23 01:36:42 +0530

    added jwt based authentication

--- apps/api/src/app/app.route.ts
+++ apps/api/src/app/app.route.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@nestjs/core';
+import { AuthModule } from './http/auth/auth.module';
 import { UserModule } from './http/users/user.module';
 
 const appRoutes: Routes = [
   {
     path: '/api',
-    children: [{ path: '/', module: UserModule }],
+    children: [
+      { path: '/', module: UserModule },
+      { path: '/', module: AuthModule },
+    ],
   },
 ];
 export { appRoutes };
--- apps/api/src/app/http/auth/auth.module.ts
+++ apps/api/src/app/http/auth/auth.module.ts
@@ -1,13 +1,23 @@
 import { CoreUserModule } from '@core/users/core-user.module';
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { jwtConfig } from 'apps/api/src/config/auth.config';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
 import { LocalStrategy } from './local.strategy';
 
 @Module({
-  imports: [CoreUserModule, PassportModule],
+  imports: [
+    CoreUserModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConfig.secret,
+      signOptions: { expiresIn: '10000' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
 })
 export class AuthModule {}
