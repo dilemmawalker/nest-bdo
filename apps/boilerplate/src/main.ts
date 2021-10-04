0652a107890d180760baa738db245cf6002a9ee7
sauravkumar <saurav.kumar@niyotail.com>
2021-10-04 07:44:53 +0530

    removed example boilerplate apps

--- apps/boilerplate/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { BoilerplateModule } from './boilerplate.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(BoilerplateModule);
-  await app.listen(3000);
-}
-bootstrap();
