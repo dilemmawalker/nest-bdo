c1bad837c830e5aa6bc5e58951448c7d2ae68993
sauravkumar <saurav.kumar@niyotail.com>
2021-10-05 12:40:57 +0530

    add corerole module in role module of admin

--- apps/admin/src/app/http/roles/role.module.ts
+++ apps/admin/src/app/http/roles/role.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  Permission,
+  PermissionSchema,
+} from '@shared/app/schemas/users/permission.schema';
+import { Role, RoleSchema } from '@shared/app/schemas/users/roles.schema';
+import { CoreRoleModule } from 'libs/core/roles/src/core-role.module';
 import { RoleRepository } from '../../../../../../libs/core/roles/src/role.repository';
 import { RoleService } from '../../../../../../libs/core/roles/src/role.service';
 import { RoleController } from './role.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    CoreRoleModule,
+    MongooseModule.forFeature([{ name: Role.name, schema: RoleSchema }]),
+    MongooseModule.forFeature([
+      { name: Permission.name, schema: PermissionSchema },
+    ]),
+  ],
   controllers: [RoleController],
   providers: [RoleService, RoleRepository],
 })
