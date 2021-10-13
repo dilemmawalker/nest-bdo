0602d35e8383a93aacb3ab913697c5f44367f7d6
sauravkumar <saurav.kumar@niyotail.com>
2021-10-13 19:56:45 +0530

    add findOne in store repository

--- apps/admin/src/app/http/stores/store.repository.ts
+++ apps/admin/src/app/http/stores/store.repository.ts
@@ -6,11 +6,15 @@ import { StoreDto } from './dtos/store.dtos';
 
 @Injectable()
 export class StoreRepository {
-  constructor(@InjectModel(Store.name) private StoreModel: Model<Store>) {}
+  constructor(@InjectModel(Store.name) private storeModel: Model<Store>) {}
 
   async create(storeDto: StoreDto): Promise<Store> {
     console.log(StoreDto.getStore(storeDto));
-    const newStore = new this.StoreModel(StoreDto.getStore(storeDto));
+    const newStore = new this.storeModel(StoreDto.getStore(storeDto));
     return await newStore.save();
+  }
+
+  async findOne(storeId: string): Promise<Store> {
+    return await this.storeModel.findOne({ storeId });
   }
 }
