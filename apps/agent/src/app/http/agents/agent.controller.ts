1504d32004964e8c5626b9c0d7ed795a1740d331
sauravkumar <saurav.kumar@niyotail.com>
2021-11-01 13:48:29 +0530

    added status in agent store response

--- apps/agent/src/app/http/agents/agent.controller.ts
+++ apps/agent/src/app/http/agents/agent.controller.ts
@@ -40,6 +40,6 @@ export class AgentController {
   ): Promise<any> {
     const json = await this.jwtUtil.decode(auth);
     const stores = await this.agentService.getStores(json.agentId);
-    return ResponseUtils.success(StoreResponse.fromStoreArray(stores, status));
+    return ResponseUtils.success(StoreResponse.fromStoreArray(stores, status), status);
   }
 }
