3df9baf47a453fefa89ba9b79bf8413f1c1040f3
sauravkumar <saurav.kumar@niyotail.com>
2021-11-11 17:09:36 +0530

    fix the routing issue for utility module

--- apps/admin/src/app/app.route.ts
+++ apps/admin/src/app/app.route.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@nestjs/core';
-import { UtilityModule } from 'apps/agent/src/app/http/utility/utility.module';
 import { AgentModule } from './http/agent/agent.module';
 import { AuthModule } from './http/auth/auth.module';
 import { ClusterModule } from './http/clusters/cluster.module';
@@ -7,6 +6,7 @@ import { FieldModule } from './http/fields/field.module';
 import { RoleModule } from './http/roles/role.module';
 import { StoreModule } from './http/stores/store.module';
 import { UserModule } from './http/users/user.module';
+import { UtilityModule } from './http/utility/utility.module';
 import { ValidationModule } from './http/validations/validation.module';
 import { WorkflowModule } from './http/workflow/workflow.module';
 
